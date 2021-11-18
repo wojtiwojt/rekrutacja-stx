54d4ee2ec48edbfd259a48f527a62a9db5b609b2
wojtiwojt <wojti.wojt91@gmail.com>
2021-11-18 19:07:49 +0100

    books import view from google api

--- django_project/apps/book_list/urls.py
+++ django_project/apps/book_list/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path("search/", views.list_of_books_view, name="search-book"),
     path("add-book/", views.add_or_edit_book_view, name="add-book"),
     path("edit-book/<int:pk>", views.add_or_edit_book_view, name="edit-book"),
+    path("import/", views.import_books_from_api_view, name="import-books"),
+    path("delete-book/<int:pk>", views.detele_book_view, name="delete-book"),
+    path("delete-all-books/", views.delete_all_books, name="delete-all"),
 ]
